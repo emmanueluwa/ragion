22e4ca6d239684558efba3d34d504deafc4706fc
emmanuel <oemmanuel.uwadiae@gmail.com>
2025-04-25 05:27:13 +0100

    migrating notebook code to flask project files

---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you do not know the answer, say that you "
+    " do not know. Directly state the code, manual and the data source for each answer and quote the text only. state the page number and link to the source "
+    "do not add anything extra."
+    "\n\n"
+    "{context}"
+)
---
+++ store_index.py
@@ -0,0 +1,45 @@
+"""
+this file should be ran once to create index for query operation
+
+execute once unless data source is updated
+"""
+
+from src.helper import load_pdf_file, text_split, download_hugging_face_embeddings
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+
+
+extracted_data = load_pdf_file(data="data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index_name = "ragion"
+
+pc.create_index(
+    name=index_name,
+    dimension=384,
+    metric="cosine",
+    spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+)
+
+# embedding each chunk and upsert the embeddings into pinecone index
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks, index_name=index_name, embedding=embeddings
+)
