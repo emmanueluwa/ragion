c7a37e6ea307628aae2d93420d0dec32bae81877
emmanuel <oemmanuel.uwadiae@gmail.com>
2025-04-28 14:23:15 +0100

    perparing for render

--- app.py
+++ app.py
@@ -67,5 +67,9 @@ def chat():
     return str(response["answer"])
 
 
+# development mode
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080, debug=True)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run()
