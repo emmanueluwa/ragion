9126b9c465af0edae1e2dfda12ebe0509ea5be41
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2026-03-04 23:27:29 +0000

    ading disco config and updating celery config as well as dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,11 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
-COPY requirements.txt .
-# RUN pip install --no-cache-dir -r requirements.txt
-RUN pip install --no-cache-dir -r requirements.txt && pip show sentence-transformers
+RUN apt-get update && apt-get install -y gcc && rm -rf /var/lib/apt/lists/*
 
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 8080
-
-# CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:8080", "app:app"]
-
-CMD ["celery", "-A", "celery_config", "worker", "--loglevel=info", "--queues=cloud_queue"]
+EXPOSE 8000
--- celery_config.py
+++ celery_config.py
@@ -4,11 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-redis_endpoint = os.environ.get("REDIS_ENDPOINT")
-redis_port = os.environ.get("REDIS_PORT", "6379")
-redis_password = os.environ.get("REDIS_PASSWORD")
 
-redis_url = f"rediss://default:{redis_password}@{redis_endpoint}:{redis_port}?ssl_cert_reqs=CERT_NONE"
+redis_url = f"redis://localhost:6379/0"
 
 # Configure Celery
 celery_app = Celery("my_flask_app", broker=redis_url, backend=redis_url)
@@ -16,9 +13,7 @@ celery_app = Celery("my_flask_app", broker=redis_url, backend=redis_url)
 
 # Celery configuration
 celery_app.conf.update(
-    result_expires=60,  # Results expire after 60 seconds
-    redis_backend_use_ssl={"ssl_cert_reqs": None},
-    broker_use_ssl={"ssl_cert_reqs": None},
+    result_expires=3600,
     task_serializer="json",
     accept_content=["json"],
     result_serializer="json",
