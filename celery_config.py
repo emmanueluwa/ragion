43e28d2ebc31d56ba55500aaa06a882b08564c7f
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2026-03-07 00:39:51 +0000

    fix: use env vars for redis connection in celery

--- celery_config.py
+++ celery_config.py
@@ -4,8 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+redis_endpoint = os.environ.get("REDIS_ENDPOINT", "172.18.0.1")
+redis_port = os.environ.get("REDIS_PORT", "6379")
+redis_password = os.environ.get("REDIS_PASSWORD", "")
 
-redis_url = f"redis://redis:6379/0"
+if redis_password:
+    redis_url = f"redis://:{redis_endpoint}@{redis_endpoint}:{redis_port}/0"
+else:
+    redis_url = f"redis://{redis_endpoint}:{redis_port}/0"
+
 
 # Configure Celery
 celery_app = Celery("my_flask_app", broker=redis_url, backend=redis_url)
