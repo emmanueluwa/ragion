92d50f35f111dbf1ff1ad0277dd9255062279004
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2026-03-17 21:13:13 +0000

    fix: updating aws ses config

--- app.py
+++ app.py
@@ -28,7 +28,9 @@ def create_app():
     app.config["MAIL_USE_TLS"] = True
     app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
     app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
-    app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_USERNAME")
+    app.config["MAIL_DEFAULT_SENDER"] = os.environ.get(
+        "MAIL_USERNAME", "noreply@swiftciv.com"
+    )
 
     # initialise extensions
     db.init_app(app)
