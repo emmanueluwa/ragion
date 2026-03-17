7a64759009f1b70ec7960a4c785adcb5d91e1593
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2026-03-17 22:38:28 +0000

    updating mail sender

--- app.py
+++ app.py
@@ -28,9 +28,7 @@ def create_app():
     app.config["MAIL_USE_TLS"] = True
     app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
     app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
-    app.config["MAIL_DEFAULT_SENDER"] = os.environ.get(
-        "MAIL_USERNAME", "noreply@swiftciv.com"
-    )
+    app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
 
     # initialise extensions
     db.init_app(app)
